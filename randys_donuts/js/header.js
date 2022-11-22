48b68a62c63b6a7cde6c9436bb554e8be9c30dea
yeonjinzzang <111642956+yeonjinzzang@users.noreply.github.com>
2022-11-22 12:01:44 +0900

    1.8

---
+++ randys_donuts/js/header.js
@@ -0,0 +1,12 @@
+//다운네비
+$(function()
+  {$(".bar_l > li").mouseenter(function(){
+    $(".navi_H").slideDown();
+  });
+  $(".navi").mouseleave(function(){
+    $(".navi_H").slideUp();
+  });
+
+});
+
+0
